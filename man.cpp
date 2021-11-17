e15dafbf5c42cd9f489c8afb56cef72926e1823e
Tinkolik <94042598+Tinkolik@users.noreply.github.com>
2021-11-17 12:49:32 +0500

    Update man.cpp

--- man.cpp
+++ man.cpp
@@ -5,11 +5,26 @@ void man()
 {
    setfillstyle (SOLID_FILL, YELLOW);
    fillellipse(100, 500, 20, 20);
-   moveto(100, 520);
+   setfillstyle(SOLID_FILL, WHITE);
+   fillellipse(108, 495, 5, 5);
+   fillellipse(92, 495, 5, 5);
+   setfillstyle(SOLID_FILL, BLACK);
+   fillellipse(93, 495, 2,2);
+   fillellipse(107, 495, 2, 2);
+   setcolor(RED);
+   setlinestyle(0, 0, 3);
+   moveto(92, 508);
+   lineto(100, 510);
+   lineto(108, 508);
+   setcolor(WHITE);
    setlinestyle(0,0,4);
+   moveto(100, 520);
    lineto(100, 575);
    lineto(125, 600);
    moveto(100, 575);
    lineto(75, 600);
-
+   moveto(100, 545);
+   lineto(125, 555);
+   moveto(100, 545);
+   lineto(75, 555);
 }
